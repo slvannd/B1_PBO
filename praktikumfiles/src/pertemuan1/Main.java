ba8d9f2f961130c78da373a0bba68be1df593558
Aldion <aldion.sumampouw@gmail.com>
2022-03-25 23:03:24 +0800

    penambahan praktikum 1

--- praktikumfiles/src/pertemuan1/Main.java
+++ praktikumfiles/src/pertemuan1/Main.java
@@ -2,9 +2,16 @@ package pertemuan1;
 
 import pertemuan1.model.Mouse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Making object of Mouse by using primary constructor
+    public static void main(String[] args) throws IOException {
+
+        /*
+         // Making object of Mouse by using primary constructor
         Mouse logitech = new Mouse("Logitech", 146, 6, "White");
 
         // Making object of Mouse by using primary constructor
@@ -21,5 +28,52 @@ public class Main {
         System.out.println("Weight: " + logitech.getWeight());
         System.out.println("Color: " + logitech.getColor());
         System.out.println("Total Button: " + logitech.getTotalButton());
+         */
+
+        /*
+        // Making Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+        // Entering value to name with Scanner object -> keyboard
+        System.out.print("Enter name: ");
+        String name = keyboard.nextLine();
+
+        // Entering value to studentID with Scanner object -> keyboard
+        System.out.print("Enter student's ID: ");
+        String studentID = keyboard.nextLine();
+
+        // Entering value to studentGPA with Scanner object -> keyboard
+        System.out.print("Enter student's GPA: ");
+        float studentGPA = keyboard.nextFloat();
+
+        // Or entering value to studentGPA with Scanner object -> keyboard and convert to float
+        // float studentGPA = Float.parseFloat(keyboard.nextLine());
+
+        // print out the value of name, studentID, and studentGPA
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student GPA: " + studentGPA);
+
+         */
+
+        // Making input with BufferedReader and InputStreamReader
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        // Entering value to name with BufferedReader object -> keyboard
+        System.out.print("Enter name: ");
+        String name = keyboard.readLine();
+
+        // Entering value to studentID with BufferedReader object -> keyboard
+        System.out.print("Enter student's ID: ");
+        String studentID = keyboard.readLine();
+
+        // Or entering value to studentGPA with BufferedReader object -> keyboard and convert to float
+        System.out.print("Enter student's GPA: ");
+        float studentGPA = Float.parseFloat(keyboard.readLine());
+
+        // print out the value of name, studentID, and studentGPA
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student GPA: " + studentGPA);
+
     }
 }
