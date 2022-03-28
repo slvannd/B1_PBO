864c666f688d7a3f9677cf3f0378d1b27b6e16c6
YogaTriSaputra <yogacinaghdjokaam354@gmail.com>
2022-03-28 23:38:54 +0800

    Posttest1

---
+++ soalpostest/2009106106_Yoga_Tri_Saputra/src/POSTTEST1/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POSTTEST1;
+
+/**
+ *
+ * @author Asus
+ */
+public class main {
+    
+}
