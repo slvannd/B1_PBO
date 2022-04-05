999371e3e88648da02bff93a4c109116df02b1d4
RizkyAmalia1125 <rizkyamalia1125@gmail.com>
2022-04-05 23:33:01 +0800

    posttest2

---
+++ 2009106122_Rizky_Fitriani/2009106122_Rizky_Fitriani/2009106122_Rizky_Fitriani/src/Posttest2/PetShop.java
@@ -0,0 +1,29 @@
+package Posttest2;
+
+class PetShop {
+    String merk;
+    String jenis;
+    double berat;
+    double harga;
+    String kategori;
+    
+    public PetShop(String merk, String jenis, double berat, double harga, String kategori) {
+        this.merk = merk;
+        this.jenis = jenis;
+        this.berat = berat;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+    void melihatbarang() {
+        System.out.println("Melihat data barang di pet shop");
+    }
+    void menambahBarang(){
+        System.out.println("Data barang di pet shop ditambah");
+    }
+    void mengeditBarang() {
+        System.out.println("Data barang di pet shop diedit");
+    }
+    void menghapusBarang() {
+        System.out.println("Data barang di pet shop dihapus");
+    }
+}
