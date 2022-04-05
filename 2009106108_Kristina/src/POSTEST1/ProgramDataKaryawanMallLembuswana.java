ff79a9365dd48a44c4e4ca3e471e16a9e2a6d73d
Kristinakrist <krisstut99151@gmail.com>
2022-04-05 22:41:03 +0800

    POSTEST2 108

--- 2009106108_Kristina/src/POSTEST1/ProgramDataKaryawanMallLembuswana.java
+++ 2009106108_Kristina/src/POSTEST1/ProgramDataKaryawanMallLembuswana.java
@@ -1,11 +1,9 @@
-
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package POSTEST1;
 
 /**
  *
@@ -67,3 +65,4 @@ public class ProgramDataKaryawanMallLembuswana {
         System.out.println("===========================================");
     }
  }
+}
