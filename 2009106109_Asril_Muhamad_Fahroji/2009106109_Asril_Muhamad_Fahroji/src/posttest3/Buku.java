46f88bed204a34b2223e8334b0927b596d50b2c1
asrilmf10 <namikazedarman@gmail.com>
2022-04-12 21:19:35 +0800

    posttest3 109 AsrilMF

---
+++ 2009106109_Asril_Muhamad_Fahroji/2009106109_Asril_Muhamad_Fahroji/src/posttest3/Buku.java
@@ -0,0 +1,47 @@
+package posttest3;
+
+public class Buku {
+    private String namaBuku;
+    private int kodeBuku;
+    private int kategoriBuku;
+    private String warnaBuku;
+
+    public Buku(String namaBuku, int kodeBuku, int kategoriBuku, String warnaBuku) {
+        this.namaBuku = namaBuku;
+        this.kodeBuku = kodeBuku;
+        this.kategoriBuku = kategoriBuku;
+        this.warnaBuku = warnaBuku;
+    }
+
+    public String getWarnaBuku() {
+        return warnaBuku;
+    }
+
+    public void setWarnaBuku(String warnaBuku) {
+        this.warnaBuku = warnaBuku;
+    }
+
+    public String getNamaBuku() {
+        return namaBuku;
+    }
+
+    public void setNamaBuku(String namaBuku) {
+        this.namaBuku = namaBuku;
+    }
+
+    public int getKodeBuku() {
+        return kodeBuku;
+    }
+
+    public void setKodeBuku(int kodeBuku) {
+        this.kodeBuku = kodeBuku;
+    }
+
+    public int getKategoriBuku() {
+        return kategoriBuku;
+    }
+
+    public void setKategoriBuku(int kategoriBuku) {
+        this.kategoriBuku = kategoriBuku;
+    }
+}
