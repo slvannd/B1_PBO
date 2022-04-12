a3e1e0f5583dd26c483465f992fb57a8b2d176ed
dionpouw <aldion.sumampouw@gmail.com>
2022-04-13 07:06:37 +0800

    Delete unused folder

--- 126_Rena Indah Choirunnisa_/src/postespbo1.java
+++ 126_Rena Indah Choirunnisa_/src/postespbo1.java
@@ -7,45 +7,44 @@ import java.util.Scanner;
  */
 
 /**
- *
  * @author Rena Indah C
  */
 public class postespbo1 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-    Scanner input = new Scanner(System.in);
-    String inputuser;
-    boolean lanjut= true;
+        Scanner input = new Scanner(System.in);
+        String inputuser;
+        boolean lanjut = true;
 
-    while (lanjut) {
-    System.out.println("Persediaan Benih Sayuran Koperasi UD Jaya");
-    System.out.println("1. Lihat Persediaan Benih Sayuran");
-    System.out.println("2. Tambah Data Persediaan Benih Sayuran");
-    System.out.println("3. Ubah Data Persediaan Benih Sayuran");
-    System.out.println("4. Hapus Data Persediaan Benih Sayuran");
+        while (lanjut) {
+            System.out.println("Persediaan Benih Sayuran Koperasi UD Jaya");
+            System.out.println("1. Lihat Persediaan Benih Sayuran");
+            System.out.println("2. Tambah Data Persediaan Benih Sayuran");
+            System.out.println("3. Ubah Data Persediaan Benih Sayuran");
+            System.out.println("4. Hapus Data Persediaan Benih Sayuran");
 
-    System.out.print("Masukkan Pilihan Anda : ");
-    inputuser = input.next();
+            System.out.print("Masukkan Pilihan Anda : ");
+            inputuser = input.next();
 
-    switch (inputuser) {
-        case "1":
-            System.out.println("Data Seluruh Benih Sayuran");
-            break;
-        case "2":
-            System.out.println("Tambah Data");
-            break;
+            switch (inputuser) {
+                case "1":
+                    System.out.println("Data Seluruh Benih Sayuran");
+                    break;
+                case "2":
+                    System.out.println("Tambah Data");
+                    break;
 
-        case "3":
-            System.out.println("Ubah Data");
-            break;
-        case "4":
-            System.out.println("Hapus Data");
-            break;
-        default:
-            System.out.println("Pilihan Tidak Ditemukan");
+                case "3":
+                    System.out.println("Ubah Data");
+                    break;
+                case "4":
+                    System.out.println("Hapus Data");
+                    break;
+                default:
+                    System.out.println("Pilihan Tidak Ditemukan");
+            }
+
+        }
+
     }
-
-}
-        
-}
 }
